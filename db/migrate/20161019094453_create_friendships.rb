696920594fea861f8f63c8ed79eac11c6c0fe696
LDENV Ruby <dietertim@mac.com>
2016-10-19 16:13:36 +0200

    Add friendship and search functionality

--- db/migrate/20161019094453_create_friendships.rb
+++ db/migrate/20161019094453_create_friendships.rb
@@ -3,8 +3,6 @@ class CreateFriendships < ActiveRecord::Migration
     create_table :friendships do |t|
       t.belongs_to :user
       t.belongs_to :friend, class: 'User'
-
-
       t.timestamps null: false
     end
   end
